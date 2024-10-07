1906c57f472a45a7c6561c342cb8da74bfd745c8
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-10-07 15:07:55 +0200

    Fix order tracking to display 'Preparing' then 'Baking' the 'Out for Delivery

--- src/pages/OrderTrackingPage.js
+++ src/pages/OrderTrackingPage.js
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const OrderTrackingPage = () => {
   const [orderStatus, setOrderStatus] = useState("Preparing");
 
-  setTimeout(() => {
-    setOrderStatus("Baking");
-  }, 3000);
+  useEffect(() => {
+    const timers = [];
 
-  setTimeout(() => {
-    setOrderStatus("Out for Delivery");
-  }, 9000);
+    timers.push(
+      setTimeout(() => {
+        setOrderStatus("Baking");
+      }, 3000)
+    );
+
+    timers.push(
+      setTimeout(() => {
+        setOrderStatus("Out for Delivery");
+      }, 7000)
+    );
+
+    return () => {
+      timers.forEach((timer) => clearTimeout(timer));
+    };
+  }, []);
 
   return (
     <div>
