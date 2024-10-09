1626a0185e8f6f1d7b9f2fd198c1493db32036f7
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-10-09 14:25:44 +0200

    Worked on the styling for the home page as well as the content for the page and the logo is also added to the page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,27 @@
 import React from "react";
+import "../styles/HomeStyles.css";
+import logo from "../images/logo/SliceHub.png";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to SliceHub</h1>
-      <p>Your one-stop solution for ordering delicious pizzas!</p>
+    <div className="homepage">
+      <header className="homepage-header">
+        <div className="header-content">
+          <img src={logo} alt="SliceHub Logo" className="logo" />
+          <h1>Welcome to SliceHub</h1>
+        </div>
+        <p className="tagline">
+          Your favorite pizzas delivered fresh to your door!
+        </p>
+      </header>
+      <section className="features">
+        <h2>Why Choose SliceHub?</h2>
+        <p>
+          Experience the best pizzas in town. Order now and enjoy fresh,
+          delicious flavors delivered to your doorstep!
+        </p>
+        <button className="order-button">Order Now</button>
+      </section>
     </div>
   );
 };
