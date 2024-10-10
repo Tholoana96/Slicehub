b781817b7d51b49674ff6c759d3884da97b8a9dd
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-10-10 15:23:25 +0200

    Added animations to the Order tracking page

--- src/pages/OrderTrackingPage.js
+++ src/pages/OrderTrackingPage.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
+import Lottie from "react-lottie";
 import "../styles/OrderTrackingStyles.css";
+import preparingAnimation from "../animations/preparing.json";
+import bakingAnimation from "../animations/baking.json";
+import deliveryAnimation from "../animations/delivery.json";
 
 const OrderTrackingPage = () => {
   const [orderStatus, setOrderStatus] = useState("Preparing");
@@ -10,13 +14,13 @@ const OrderTrackingPage = () => {
     timers.push(
       setTimeout(() => {
         setOrderStatus("Baking");
-      }, 3000)
+      }, 5000)
     );
 
     timers.push(
       setTimeout(() => {
         setOrderStatus("Out for Delivery");
-      }, 7000)
+      }, 10000)
     );
 
     return () => {
@@ -24,10 +28,51 @@ const OrderTrackingPage = () => {
     };
   }, []);
 
+  const animationOptions = (animationData) => ({
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  });
+
+  const renderAnimation = () => {
+    switch (orderStatus) {
+      case "Preparing":
+        return (
+          <Lottie
+            options={animationOptions(preparingAnimation)}
+            height={300}
+            width={300}
+          />
+        );
+      case "Baking":
+        return (
+          <Lottie
+            options={animationOptions(bakingAnimation)}
+            height={300}
+            width={300}
+          />
+        );
+      case "Out for Delivery":
+        return (
+          <Lottie
+            options={animationOptions(deliveryAnimation)}
+            height={300}
+            width={300}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="tracking">
       <h1>Order Tracking</h1>
       <p>Current Status: {orderStatus}</p>
+      <div className="animation-container">{renderAnimation()}</div>
     </div>
   );
 };
