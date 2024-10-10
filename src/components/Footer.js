3f2b00195ba33d283c11d75afd58e5115badfd1d
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-10-10 15:24:27 +0200

    Worked on making the footer more realistic to a real pizza web application

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,29 +5,33 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; 2024 SliceHub. All rights reserved.</p>
       <br />
-      <div className="contact-info">
-        <p>Email: info@slicehub.co.za</p>
-        <p>Phone: 011-889-9909</p>
-      </div>
-      <br />
+      <p>
+        Follow us on social media for the latest updates, promotions, and more!
+      </p>
       <div className="social-icons">
         <i className="fab fa-instagram"></i>
         <i className="fab fa-linkedin-in"></i>
         <i className="fab fa-facebook"></i>
       </div>
       <br />
+      <div className="contact-info">
+        <p>Email: info@slicehub.co.za</p>
+        <p>Phone: 011-889-9909</p>
+      </div>
+      <br />
       <div className="additional-info">
-        <p>
-          Follow us on social media for the latest updates, promotions, and
-          more!
-        </p>
         <p>
           123 Pizza Lane, Flavor Town, SA | Open Monday to Saturday, 9 AM - 10
           PM
         </p>
       </div>
+
+      <br />
+      <hr />
+      <br />
+
+      <p>&copy; 2024 SliceHub. All rights reserved.</p>
     </footer>
   );
 };
