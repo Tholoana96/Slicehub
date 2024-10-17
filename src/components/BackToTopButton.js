582b94dd45cdf44f3cbcdd94f43a488bd5f8b09a
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-10-17 21:46:28 +0200

    Styled the back to top button and the footer

--- src/components/BackToTopButton.js
+++ src/components/BackToTopButton.js
@@ -31,8 +31,15 @@ const BackToTopButton = () => {
     <div>
       {visible && (
         <button onClick={scrollToTop} className="backToTopButton">
-          {" "}
-          Back to Top
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="white"
+            width="24px"
+            height="24px"
+          >
+            <path d="M12 5l7 7-1.41 1.41L12 7.83l-5.59 5.58L5 12l7-7zm0 7l7 7-1.41 1.41L12 14.83l-5.59 5.58L5 19l7-7z" />
+          </svg>
         </button>
       )}
     </div>
