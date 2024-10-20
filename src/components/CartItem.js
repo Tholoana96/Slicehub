e5b6db9dcba636ae5f1657cd1a04f100824a4e77
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-10-20 18:23:50 +0200

    Added horizontal line under each pizza item

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -7,31 +7,33 @@ const CartItem = ({
   updateQuantity,
 }) => (
   <div className="cart-item">
-    <div className="cart-item-content">
-      <img src={image} alt={name} className="cart-item-image" />
-      <div className="cart-item-details">
-        <h4 className="item-name">{name}</h4>
-        <p className="price">Price: R{price.toFixed(2)}</p>
-        <div className="quantity-actions">
-          <div className="cart-item-actions">
-            <button
-              className="small-button"
-              onClick={() => updateQuantity(Math.max(quantity - 1, 1))}
-              disabled={quantity <= 1}
-            >
-              -
-            </button>
-            <span>{quantity}</span>
-            <button
-              className="small-button"
-              onClick={() => updateQuantity(quantity + 1)}
-            >
-              +
+    <div class="cart-item-wrapper">
+      <div className="cart-item-content">
+        <img src={image} alt={name} className="cart-item-image" />
+        <div className="cart-item-details">
+          <h4 className="item-name">{name}</h4>
+          <p className="price">Price: R{price.toFixed(2)}</p>
+          <div className="quantity-actions">
+            <div className="cart-item-actions">
+              <button
+                className="small-button"
+                onClick={() => updateQuantity(Math.max(quantity - 1, 1))}
+                disabled={quantity <= 1}
+              >
+                -
+              </button>
+              <span>{quantity}</span>
+              <button
+                className="small-button"
+                onClick={() => updateQuantity(quantity + 1)}
+              >
+                +
+              </button>
+            </div>
+            <button onClick={removeFromCart} className="remove-button">
+              Remove
             </button>
           </div>
-          <button onClick={removeFromCart} className="remove-button">
-            Remove
-          </button>
         </div>
       </div>
     </div>
