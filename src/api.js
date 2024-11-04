14b916083f418242745d2bd2c649147541944a65
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-11-04 17:17:04 +0200

    Added file for deployment error

---
+++ src/api.js
@@ -0,0 +1,19 @@
+const BASE_URL = "https://slicehub-1.onrender.com/api";
+
+export const registerUser = async (userData) => {
+  const response = await fetch(`${BASE_URL}/users/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userData),
+  });
+  return response.json();
+};
+
+export const loginUser = async (credentials) => {
+  const response = await fetch(`${BASE_URL}/users/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+  });
+  return response.json();
+};
